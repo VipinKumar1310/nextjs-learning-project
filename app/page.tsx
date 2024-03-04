46c7e4707357c0e6f69bee44a0c0908d18acebe9
VipinKumar1310 <vk22914916@gmail.com>
2024-03-04 15:08:27 +0530

    deploying error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from '@/app/ui/home.module.css';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page() {
   return (
